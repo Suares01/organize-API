960783dd25af5b2485fe16401dee917932f7b97b
Suares01 <hub_suares.01@hotmail.com>
2022-02-18 19:02:53 -0300

    Database connection config

---
+++ src/database/index.ts
@@ -0,0 +1,24 @@
+import config from "config";
+import mongoose, { Mongoose } from "mongoose";
+
+import { Database } from "@config/types";
+import logger from "@src/logger/logger";
+
+const database = config.get<Database>("App.database");
+
+mongoose.connection.on("error", (error) =>
+  logger.error(`Database error: ${error}`)
+);
+
+mongoose.connection.on("open", () => logger.info("Database connected"));
+
+mongoose.connection.on("disconnected", () =>
+  logger.info("Database disconnected")
+);
+
+export const connect = async (): Promise<Mongoose> =>
+  await mongoose.connect(database.uri);
+
+export const disconnect = async (): Promise<void> => {
+  await mongoose.connection.close();
+};
