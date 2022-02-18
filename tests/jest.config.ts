fac9641a5844fe98e5b4465792c5aa5a9550be05
Suares01 <hub_suares.01@hotmail.com>
2022-02-17 21:14:19 -0300

    Integration tests config

---
+++ tests/jest.config.ts
@@ -0,0 +1,15 @@
+import rootConfig from "../jest.config";
+
+const config: import("@jest/types").Config.InitialOptions = {
+  ...rootConfig,
+  displayName: "integration tests",
+  setupFilesAfterEnv: ["<rootDir>/jestSetup.ts"],
+  testMatch: ["<rootDir>/**/*.spec.ts"],
+  moduleNameMapper: {
+    "@src/(.*)": "<rootDir>/../src/$1",
+    "@config/(.*)": "<rootDir>/../config/$1",
+    "@tests/(.*)": "<rootDir>/$1",
+  },
+};
+
+export default config;
