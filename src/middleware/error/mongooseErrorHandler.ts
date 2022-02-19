be2cffd3f596164efc8d6c3de1a9786bdac64592
Suares01 <hub_suares.01@hotmail.com>
2022-02-19 13:16:10 -0300

    Mongoose error handler middleware

---
+++ src/middleware/error/mongooseErrorHandler.ts
@@ -0,0 +1,27 @@
+import { NextFunction, Request, Response } from "express";
+import mongoose from "mongoose";
+
+import { ApiError } from "@src/shared/errors/ApiError";
+import { ErrorUtil } from "@src/shared/errors/ErrorUtil";
+
+export function mongooseErrorHandler(
+  error: unknown,
+  _: Request,
+  res: Response,
+  next: NextFunction
+): Response | void {
+  const { handleValidationError } = ErrorUtil;
+
+  if (error instanceof mongoose.Error.ValidationError) {
+    const { code, message } = handleValidationError(error);
+
+    return res.status(code).send(
+      ApiError.format({
+        code,
+        message,
+      })
+    );
+  }
+
+  return next(error);
+}
