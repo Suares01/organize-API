8e9291d83e8625417fa5923bf614130c705e5d91
Suares01 <hub_suares.01@hotmail.com>
2022-02-21 18:26:55 -0300

    Register ProjectsRepository like singleton

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,5 +1,7 @@
 import { container } from "tsyringe";
 
+import { ProjectsRepository } from "@modules/projects/repositories/implementations/ProjectsRepository";
 import { UsersRepository } from "@modules/users/repositories/implementations/UsersRepository";
 
 container.registerSingleton("UsersRepository", UsersRepository);
+container.registerSingleton("ProjectsRepository", ProjectsRepository);
