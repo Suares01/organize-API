7cb395a186d06f4e1174bdd7c25ec05dcf4b7a44
Suares01 <hub_suares.01@hotmail.com>
2022-02-21 18:27:42 -0300

    Implement tsyringe

--- src/modules/projects/useCases/CreateProjectUseCase.ts
+++ src/modules/projects/useCases/CreateProjectUseCase.ts
@@ -1,9 +1,15 @@
+import { inject, injectable } from "tsyringe";
+
 import { IProjectDto } from "@modules/projects/dtos/IProjectDto";
 import { IProject } from "@modules/projects/models/Project";
 import { IProjectsRepository } from "@modules/projects/repositories/IProjectsRepository";
 
+@injectable()
 export class CreateProjectUseCase {
-  constructor(private projectsRepository: IProjectsRepository) {}
+  constructor(
+    @inject("ProjectsRepository")
+    private projectsRepository: IProjectsRepository
+  ) {}
 
   async execute(data: IProjectDto): Promise<IProject> {
     const project = await this.projectsRepository.insert(data);
