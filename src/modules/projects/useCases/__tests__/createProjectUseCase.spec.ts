fa9d940ac5296cb60310ed2c4616856d95f6e9c3
Suares01 <hub_suares.01@hotmail.com>
2022-02-22 21:30:43 -0300

    Add throw ConflictError test

--- src/modules/projects/useCases/__tests__/createProjectUseCase.spec.ts
+++ src/modules/projects/useCases/__tests__/createProjectUseCase.spec.ts
@@ -1,7 +1,10 @@
+import "reflect-metadata";
+
 import { IProjectDto } from "@modules/projects/dtos/IProjectDto";
 import { IProject } from "@modules/projects/models/Project";
 import { ProjectsRepository } from "@modules/projects/repositories/implementations/ProjectsRepository";
 import { CreateProjectUseCase } from "@modules/projects/useCases/CreateProjectUseCase";
+import { ConflictError } from "@src/shared/errors/internalErrors/ConflictError";
 
 jest.mock("@modules/projects/repositories/implementations/ProjectsRepository");
 
@@ -12,6 +15,7 @@ describe("Create Project Use Case tests", () => {
   const newProject: IProjectDto = {
     name: "new-api",
     path: "path/example",
+    user_id: "1fd5s61fsdf0",
   };
 
   const createdProjet: IProject = {
@@ -31,5 +35,20 @@ describe("Create Project Use Case tests", () => {
     const project = await createProjectUseCase.execute(newProject);
 
     expect(project).toEqual(createdProjet);
+  });
+
+  it("should throw ConflictError if the user already has a project with the given name", async () => {
+    mockedProjectsRepository.findOne.mockResolvedValue(createdProjet);
+
+    const createProjectUseCase = new CreateProjectUseCase(
+      mockedProjectsRepository
+    );
+
+    await expect(
+      createProjectUseCase.execute(newProject)
+    ).rejects.toBeInstanceOf(ConflictError);
+    await expect(createProjectUseCase.execute(newProject)).rejects.toThrow(
+      new ConflictError("Project already exists")
+    );
   });
 });
