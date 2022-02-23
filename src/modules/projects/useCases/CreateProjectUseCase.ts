634110ba36eb7dd0c63985217d86ff7dcd6abe7b
Suares01 <hub_suares.01@hotmail.com>
2022-02-22 21:31:44 -0300

    verify if user already has a project

--- src/modules/projects/useCases/CreateProjectUseCase.ts
+++ src/modules/projects/useCases/CreateProjectUseCase.ts
@@ -3,6 +3,7 @@ import { inject, injectable } from "tsyringe";
 import { IProjectDto } from "@modules/projects/dtos/IProjectDto";
 import { IProject } from "@modules/projects/models/Project";
 import { IProjectsRepository } from "@modules/projects/repositories/IProjectsRepository";
+import { ConflictError } from "@src/shared/errors/internalErrors/ConflictError";
 
 @injectable()
 export class CreateProjectUseCase {
@@ -12,6 +13,10 @@ export class CreateProjectUseCase {
   ) {}
 
   async execute(data: IProjectDto): Promise<IProject> {
+    const projectAlreadyExists = await this.projectsRepository.findOne(data);
+
+    if (projectAlreadyExists) throw new ConflictError("Project already exists");
+
     const project = await this.projectsRepository.insert(data);
 
     return project;
