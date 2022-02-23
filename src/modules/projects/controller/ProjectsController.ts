7b2480743c6930a3d001e1202d553f664e6258d9
Suares01 <hub_suares.01@hotmail.com>
2022-02-22 21:34:10 -0300

    Add authUserMiddleware

--- src/modules/projects/controller/ProjectsController.ts
+++ src/modules/projects/controller/ProjectsController.ts
@@ -1,20 +1,23 @@
 import { Request, Response } from "express";
 import { container } from "tsyringe";
 
-import { Controller, Post } from "@overnightjs/core";
+import { ClassMiddleware, Controller, Post } from "@overnightjs/core";
+import { authUserMiddleware } from "@src/middleware/auth/authUser";
 
 import { IProjectDto } from "../dtos/IProjectDto";
 import { CreateProjectUseCase } from "../useCases/CreateProjectUseCase";
 
 @Controller("projects")
+@ClassMiddleware(authUserMiddleware)
 export class ProjectsController {
   @Post("")
   async create(req: Request, res: Response): Promise<Response> {
     const { name, path } = req.body as IProjectDto;
+    const { id } = req.user;
 
     const project = await container
       .resolve(CreateProjectUseCase)
-      .execute({ name, path });
+      .execute({ name, path, user_id: id });
 
     return res.status(201).send(project);
   }
