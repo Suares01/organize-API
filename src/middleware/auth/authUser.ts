7036e7d5d4784119c74fa0b691d14347beab4ba0
Suares01 <hub_suares.01@hotmail.com>
2022-02-22 21:32:43 -0300

    Auth user middleware

---
+++ src/middleware/auth/authUser.ts
@@ -0,0 +1,39 @@
+import { NextFunction, Request, Response } from "express";
+import { container } from "tsyringe";
+
+import { UsersRepository } from "@src/modules/users/repositories/implementations/UsersRepository";
+import { UnauthorizedError } from "@src/shared/errors/internalErrors/UnauthorizedError";
+import { verifyJwt } from "@src/shared/util/token";
+
+interface IPayLoad {
+  sub: string;
+}
+
+export async function authUserMiddleware(
+  req: Request,
+  _: Response,
+  next: NextFunction
+): Promise<void> {
+  const token = req.headers?.["x-access-token"];
+
+  if (!token || typeof token !== "string")
+    throw new UnauthorizedError("jwt must be provided");
+
+  try {
+    const { sub: userId } = (await verifyJwt(token)) as IPayLoad;
+
+    const usersRepository = container.resolve(UsersRepository);
+
+    const user = await usersRepository.findOne({ id: userId });
+
+    if (!user) throw new UnauthorizedError("User not found");
+
+    req.user = {
+      id: userId,
+    };
+
+    next();
+  } catch (error: any) {
+    throw new UnauthorizedError(error.message);
+  }
+}
