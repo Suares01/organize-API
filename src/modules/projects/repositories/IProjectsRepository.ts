2618d30903aca7ebe32a8a9620b2bdfa55175437
Suares01 <hub_suares.01@hotmail.com>
2022-02-25 18:54:02 -0300

    Add IFindOne interface

--- src/modules/projects/repositories/IProjectsRepository.ts
+++ src/modules/projects/repositories/IProjectsRepository.ts
@@ -1,7 +1,12 @@
 import { IProjectDto } from "@modules/projects/dtos/IProjectDto";
 import { IProject } from "@modules/projects/models/Project";
 
+export interface IFindOne {
+  name: string;
+  user_id: string;
+}
+
 export interface IProjectsRepository {
   insert(data: IProjectDto): Promise<IProject>;
-  findOne(data: IProjectDto): Promise<IProject | null>;
+  findOne(data: IFindOne): Promise<IProject | null>;
 }
