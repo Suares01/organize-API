193cea4f62059c7ef49338aeb8b7d2a9210f189c
Suares01 <hub_suares.01@hotmail.com>
2022-02-25 18:22:19 -0300

    Add @shared/@middleware in moduleNameMapper

--- tests/jest.config.ts
+++ tests/jest.config.ts
@@ -6,10 +6,11 @@ const config: import("@jest/types").Config.InitialOptions = {
   setupFilesAfterEnv: ["<rootDir>/jestSetup.ts"],
   testMatch: ["<rootDir>/**/*.spec.ts"],
   moduleNameMapper: {
-    "@src/(.*)": "<rootDir>/../src/$1",
+    "@middleware/(.*)": "<rootDir>/../src/middleware/$1",
     "@modules/(.*)": "<rootDir>/../src/modules/$1",
     "@config/(.*)": "<rootDir>/../config/$1",
     "@tests/(.*)": "<rootDir>/$1",
+    "@shared/(.*)": "<rootDir>/../src/shared/$1",
   },
 };
 
