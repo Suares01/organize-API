0491df51b7cd7017bf08c417a7a0bac1f313ad13
Suares01 <hub_suares.01@hotmail.com>
2022-02-25 22:53:08 -0300

    Add find specific project endpoint

--- src/modules/projects/controller/ProjectsController.ts
+++ src/modules/projects/controller/ProjectsController.ts
@@ -4,6 +4,7 @@ import { container } from "tsyringe";
 import { authUserMiddleware } from "@middleware/auth/authUser";
 import { IProjectDto } from "@modules/projects/dtos/IProjectDto";
 import { CreateProjectUseCase } from "@modules/projects/useCases/CreateProjectUseCase";
+import { FindProjectUseCase } from "@modules/projects/useCases/FindProjectUseCase";
 import { ListAllProjectsUseCase } from "@modules/projects/useCases/ListAllProjectsUseCase";
 import { ClassMiddleware, Controller, Get, Post } from "@overnightjs/core";
 
@@ -31,5 +32,17 @@ export class ProjectsController {
       .execute(id);
 
     return res.send(listOfProjects);
+  }
+
+  @Get(":name")
+  async find(req: Request, res: Response): Promise<Response> {
+    const { name } = req.params;
+    const { id } = req.user;
+
+    const project = await container
+      .resolve(FindProjectUseCase)
+      .execute({ name, user_id: id });
+
+    return res.send(project);
   }
 }
