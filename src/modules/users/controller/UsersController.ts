2da6ef2af31c5f880cb85845c3c0ecc0ef81c316
Suares01 <hub_suares.01@hotmail.com>
2022-03-09 20:49:46 -0300

    Add /users/me endpoint

--- src/modules/users/controller/UsersController.ts
+++ src/modules/users/controller/UsersController.ts
@@ -1,10 +1,12 @@
 import { Request, Response } from "express";
 import { container } from "tsyringe";
 
+import { authUserMiddleware } from "@middleware/auth/authUser";
 import { IUserDto } from "@modules/users/dtos/IUserDto";
 import { AuthenticateUserUseCase } from "@modules/users/useCases/AuthenticateUserUseCase";
 import { CreateUserUseCase } from "@modules/users/useCases/CreateUserUseCase";
-import { Controller, Post } from "@overnightjs/core";
+import { GetUserInfoUseCase } from "@modules/users/useCases/GetUserInfoUseCase";
+import { Controller, Get, Middleware, Post } from "@overnightjs/core";
 
 @Controller("users")
 export class UsersController {
@@ -39,6 +41,24 @@ export class UsersController {
 
     return res.status(200).send({
       token,
+    });
+  }
+
+  @Get("me")
+  @Middleware(authUserMiddleware)
+  async getInfo(req: Request, res: Response): Promise<Response> {
+    const { id } = req.user;
+
+    const {
+      id: userID,
+      username,
+      created_at: createdAt,
+    } = await container.resolve(GetUserInfoUseCase).execute(id);
+
+    return res.send({
+      id: userID,
+      username,
+      created_at: createdAt,
     });
   }
 }
